ad1e8905e60a2b48aa6e5f9fee03b970b7a16d55
Jesse Marder <jesse.d.marder@gmail.com>
2022-03-09 20:21:16 -0600

    Copy over and update files

--- prepare.py
+++ prepare.py
@@ -3,34 +3,15 @@ import numpy as np
 from env import get_db_url
 from sklearn.model_selection import train_test_split
 
-
-
-def prep_iris(iris):
-    iris = iris.drop(columns=['species_id','measurement_id'])
-    iris = iris.rename(columns={'species_name':'species'})
-    dummy_df = pd.get_dummies(iris[['species']], dummy_na=False, drop_first=[True])
-    iris = pd.concat([iris, dummy_df], axis = 1)
-    iris = iris.drop(columns=['species'])
-
-    return iris
-
-def prep_titantic(df):
-    df = df.drop(columns=['passenger_id','embarked','deck','class','age'])
-    df.embark_town = df.embark_town.fillna('Southampton')
-    dummy_df = pd.get_dummies(df[['sex','embark_town']], dummy_na=False, drop_first=True)
-    df = pd.concat([df,dummy_df], axis = 1)
-    
-    return df.drop(columns=['sex','embark_town'])
-
 def prep_telco(df):
-    # replace whitespace only cells with nan
-    df = df.replace(" ",np.nan)
-    # Drop the rows with NAs 
-    df = df.dropna()
-    # Change total_charges type to float
-    df.total_charges = df.total_charges.astype('float64')
     # Drop unnecessary foreign key ids
     df = df.drop(columns=['payment_type_id','internet_service_type_id','contract_type_id'])
+    # From prior exploration of dataset a small number of the total_charges are just whitespace - these are all new customers who haven't been with the company for >1 month.
+    # Given that it is a very small proportion of the total dataset these rows will be deleted for ease of computation later on
+    df = df.drop(df[df.total_charges == " "].index)
+    # Convert total_charges to float for later analysis
+    df.total_charges = df.total_charges.astype('float64')
+
     # Determine the categorical variables - here defined as object data type (non-numeric) and with fewer than 5 values
     catcol = df.columns[(df.nunique()<5)&(df.dtypes == 'object')]
     # Encode categoricals
